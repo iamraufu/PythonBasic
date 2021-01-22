abf13b27cdff985e432555f1801c2c8d17096a83
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-22 12:07:47 +0600

    initial commit

---
+++ string.py
@@ -0,0 +1,11 @@
+company = "Raufu Prezens Inc."
+
+print(company.lower())
+print(company.upper())
+
+print(company.isupper())
+print(company.islower())
+
+print(company.upper().isupper())
+
+print(len(company))
---
+++ variable.py
@@ -0,0 +1,12 @@
+name = "Raufu"
+age = 21
+isMale = True
+print("Hey I am "+name+". I am ", age, "years old.")
+
+print("Hello "+name)
+print("I am",name,"and I am",age,"years old\nRaufu Prezens\' Gaming PC")
+
+print(type(name))
+print(type(age))
+print(type(isMale))
+
