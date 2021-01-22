e70c8e90b21e6ce6c2e4ccfc3fc032f002dea3cc
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-22 12:54:06 +0600

    first commit

---
+++ numbers.py
@@ -0,0 +1,35 @@
+from math import *
+
+print(3*4)
+
+print(3*4.75)
+
+print(3*(4.75*2))
+
+print(3*(4/2))
+
+print(3*(5%2))
+
+num = 5
+print(type(num))
+
+string = str(num)
+print(type(string))
+
+negNum = -7
+print(negNum)
+print(abs(negNum))
+
+print(pow(3, 2))
+
+print(max(7, 11))
+print(min(7, 11))
+
+print(round(4.7))
+print(round(4.2))
+
+print(floor(6.99))
+print(ceil(6.99))
+
+print(sqrt(49))
+
