ad6a1ffe734de55242985ef94a29556d7f3d20ad
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-23 12:31:14 +0600

    first commit

---
+++ calculator.py
@@ -0,0 +1,4 @@
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
--- input.py
+++ input.py
@@ -2,5 +2,3 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 
 print("Hello " + name + "! You are " + age)
-
-
