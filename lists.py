88f24fc5e5aecad064d8846bde877138be5b3b6b
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-31 00:01:13 +0600

    lists

---
+++ lists.py
@@ -0,0 +1,14 @@
+friends = ["KewNai", "AmiEka", "UmPoor", "Besh"]
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print('------')
+print(friends[-1])
+print(friends[-2])
+print(friends[-3])
+
+print(friends[1:3])
+
+friends[1] = "EkdinOnekBoroHobo"
+print(friends[1])
