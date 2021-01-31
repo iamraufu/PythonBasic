ae80150a9f46461270c0a4c12017655088d16da4
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-31 09:33:38 +0600

    lists

--- lists.py
+++ lists.py
@@ -1,14 +1,28 @@
-friends = ["KewNai", "AmiEka", "UmPoor", "Besh"]
-
-print(friends[0])
-print(friends[1])
-print(friends[2])
-print('------')
-print(friends[-1])
-print(friends[-2])
-print(friends[-3])
-
-print(friends[1:3])
-
-friends[1] = "EkdinOnekBoroHobo"
-print(friends[1])
+friends = ["Tom Hanks", "Jessy", "Walter White", "Arthur"]
+num = [12, 8, 4, 1, 25, 15, 19, 42, 23, 0]
+# print(friends[0])
+# print(friends[1])
+# print(friends[2])
+# print('------')
+# print(friends[-1])
+# print(friends[-2])
+# print(friends[-3])
+#
+# print(friends[1:3])
+#
+# friends[1] = "Thomas"
+# print(friends[1])
+#
+# friends.insert((0, "Harry"))
+# friends.remove("Tom Hanks")
+# friends.clear()
+# friends.pop()
+# print(friends.index("Jessy"))
+num.sort()
+print(num)
+friends.sort()
+print(friends)
+num.reverse()
+print(num)
+friends2 = friends.copy()
+print(friends2)
