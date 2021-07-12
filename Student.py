2bae184459bf14a70304db7392ae1a2a19d9fcdd
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-07-12 20:19:12 +0600

    class object

---
+++ Student.py
@@ -0,0 +1,8 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+
---
+++ class_object.py
@@ -0,0 +1,5 @@
+from Student import Student
+
+student1 = Student("Raufu", "CSE", "3.34", False)
+student2 = Student("Babla", "EEE", "2.5", True)
+print(student1.gpa)
