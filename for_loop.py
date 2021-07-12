c764e64c6b8a6eb5e2cfb9ce3f819b86148a0030
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-07-12 13:00:05 +0600

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+for letter in "Raufu Prezens Inc.":
+    print(letter)
+
+friends = ["Raufu", "Prezens", "Eftykhar"]
+for name in friends:
+    print(name)
+
+for index in range(10):
+    print(index)
+
+for index in range(5, 7):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for i in range(3):
+    if i == 0:
+        print("First loop")
+    else:
+        print("Not First")
